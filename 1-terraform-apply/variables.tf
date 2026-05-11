f291d193b10a249056983a538f5f9d47578dfa87
Milan Sas <milan.sas@rt-rk.com>
2026-05-11 09:31:25 +0200

    Changed bucket name

--- 1-terraform-apply/variables.tf
+++ 1-terraform-apply/variables.tf
@@ -23,6 +23,6 @@ variable "aws_endpoint" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "Bucket McBucket"
+  default     = "Bucket-McBucket"
 }
 
